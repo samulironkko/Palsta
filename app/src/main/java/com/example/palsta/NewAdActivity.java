7d59ab72f4f0b087efc654635f77fa88b3b1019c
samulironkko <37826096+samulironkko@users.noreply.github.com>
2019-03-25 10:33:46 +0200

    Studio päivitetty

---
+++ app/src/main/java/com/example/palsta/NewAdActivity.java
@@ -0,0 +1,13 @@
+package com.example.palsta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_ad);
+    }
+}
