27a40e57471af55bca9db5000e5f469ab4652a80
samulironkko <37826096+samulironkko@users.noreply.github.com>
2019-03-26 13:55:18 +0200

    Permissionit karttaan

--- app/src/main/java/com/example/palsta/NewAdActivity.java
+++ app/src/main/java/com/example/palsta/NewAdActivity.java
@@ -2,12 +2,25 @@ package com.example.palsta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewAdActivity extends AppCompatActivity {
+
+    Button mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_ad);
+
+        mapButton = findViewById(R.id.address_button);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
