84c31c06740529f81caedc7fb4b8766c978777a9
p3kka123 <p0kemonisftw@hotmail.com>
2019-03-26 11:37:14 +0200

    ui for list

--- app/src/main/java/com/example/palsta/MainActivity.java
+++ app/src/main/java/com/example/palsta/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.palsta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<AdPart> AdParts = new ArrayList<>();
+
+    ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.adList);
+        for (int i = 0 ; i < 10 ; i++) {
+            AdPart part = new AdPart();
+            AdParts.add(part);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AdAdapter adapter = new AdAdapter(this, AdParts);
+        listView.setAdapter(adapter);
     }
 }
