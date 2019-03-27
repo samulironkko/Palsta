6ebc17cd2bd09887dbb30d01a70db29a859d925d
samulironkko <37826096+samulironkko@users.noreply.github.com>
2019-03-27 14:32:58 +0200

    Place pickerin runko lisätty

--- app/src/main/java/com/example/palsta/MainActivity.java
+++ app/src/main/java/com/example/palsta/MainActivity.java
@@ -54,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-/*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
-/*
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.newAd){
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
-*/
+
     @Override
     protected void onResume() {
         super.onResume();
