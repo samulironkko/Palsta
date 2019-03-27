08c4fd5d5e2cff5f341268eedb52c33cae4a1f32
Kemppis <ilari.kemppainen@live.fi>
2019-03-27 13:20:13 +0200

    database connection

--- app/src/main/java/com/example/palsta/MainActivity.java
+++ app/src/main/java/com/example/palsta/MainActivity.java
@@ -58,10 +58,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        //inflater.inflate(R.menu.menu, menu);
         return true;
     }
-
+/*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.newAd){
